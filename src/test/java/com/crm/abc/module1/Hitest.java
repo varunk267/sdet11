2b7610469c1582f9d3646ff42227b9fb6f58c001
varunk267 <79696508+varunk267@users.noreply.github.com>
2021-03-20 12:19:34 +0530

    sdet11 first commit

---
+++ src/test/java/com/crm/abc/module1/Hitest.java
@@ -0,0 +1,13 @@
+package com.crm.abc.module1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Hitest {
+@Test
+public void hitest()
+{
+	WebDriver driver=new FirefoxDriver();
+}
+}
